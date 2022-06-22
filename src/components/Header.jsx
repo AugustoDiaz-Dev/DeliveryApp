934a9391b4ceab807d4505e91aa31208a4762725
Augusto <augustodiaz_1@hotmail.com>
2022-06-22 21:00:20 +1000

    Firebase auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { BsCart4 } from "react-icons/bs";
+import { BsCart4 } from "react-icons/bs"
+import { motion } from 'framer-motion'
 
 import Logo from './img/logo.png'
 import Avatar from './img/avatar.png'
+import { Link } from 'react-router-dom';
+
+import { app } from '../firebase.config'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 
 const Header = () => {
+
+    const firebaseAuth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const login = async () => {
+        const response = await signInWithPopup(firebaseAuth, provider);
+        console.log(response);
+    }
+
     return (
         <header className='fixed z-50 w-screen p-6 px-16'>
             {/* Desktop and tablet */}
             <div className='hidden md:flex w-full h-full items-center justify-center'></div>
-            <div className='flex items-center gap-2'>
+            <Link to={'/'} className='flex items-center gap-2'>
                 <img src={Logo} className='w-8 object-cover' alt='logo' />
                 <p className='text-headingColor text-xl font-bold'>City</p>
-            </div>
+            </Link>
             <div className='flex items-center gap-8'>
                 <ul className='flex items-center gap-8 ml-auto'>
                     <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer '>Home</li>
@@ -28,7 +42,11 @@ const Header = () => {
                     </div>
                 </div>
 
-                <img src={Avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl' alt='userprofile' />
+                <div className='relative'>
+                    <motion.img whileTap={{ scale: 0.6 }} src={Avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer' alt='userprofile'
+                        onClick={login} />
+                </div>
+
 
             </div>
 
