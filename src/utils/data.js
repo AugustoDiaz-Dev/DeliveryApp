f33e9e285ddc49f682a2b43713f7b5b365ea58d6
Augusto <augustodiaz_1@hotmail.com>
2022-06-24 18:17:32 +1000

    Data and mobile version

---
+++ src/utils/data.js
@@ -0,0 +1,19 @@
+import i1 from '../img/i1.png'
+import f1 from '../img/f1.png'
+import c3 from '../img/c3.png'
+import fi1 from '../img/fi1.png'
+
+export const heroData = [
+    {
+        id: 1, name: 'Ice Cream', description: 'Chocolate & Vanilla', price: '5.25', imageSrc: i1
+    },
+    {
+        id: 2, name: 'Strawberry', description: 'Fresh Strawberries', price: '10.25', imageSrc: f1
+    },
+    {
+        id: 3, name: 'Chicken Kebab', description: 'Mixed Kebab Plate', price: '8.25', imageSrc: c3
+    },
+    {
+        id: 4, name: 'Fish Kebab', description: 'Fish Kebab', price: '7.25', imageSrc: fi1
+    }
+]
